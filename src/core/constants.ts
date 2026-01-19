41a1db0e257323328efd1e4184a6f420e4d1769a
Joshua Patterson <joshua.patterson745@gmail.com>
2026-01-18 21:37:24 -0600

    Use a `windowStart` variable to handle cadence of caching

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -2,7 +2,7 @@ export const SHOP = process.env.SHOP;
 export const TOKEN = process.env.ACCESS_TOKEN;
 export const SHOPIFY_API_VERSION = "2026-01";
 export const REDIS_CACHE_ALL_PRODUCTS_INTERVAL = 5 * 60 * 1000; // 5 minutes
-export const REDIS_CACHE_LOCK_TTL_MS = 4 * 60 * 1000; // 4 minutes, just to be a little shorter than REDIS_CACHE_ALL_PRODUCTS_INTERVAL
+export const REDIS_CACHE_LOCK_TTL_MS = REDIS_CACHE_ALL_PRODUCTS_INTERVAL; // This is the same as REDIS_CACHE_ALL_PRODUCTS_INTERVAL to avoid multiple node instances being able to acquire the lock early.
 
 export const ALL_PRODUCTS_REDIS_KEY = "allProducts";
 export const ALL_PRODUCTS_ZSET_REDIS_KEY = ALL_PRODUCTS_REDIS_KEY + ":zset";
--- src/core/tasks.ts
+++ src/core/tasks.ts
@@ -1,34 +1,34 @@
 import { getRedisClient } from "./redis_client";
 import { LOCK_REDIS_KEY, REDIS_CACHE_LOCK_TTL_MS } from "./constants";
-import { createLock, NodeRedisAdapter, Lock } from "redlock-universal";
+import { createLock, NodeRedisAdapter } from "redlock-universal";
 
-let lock: Lock | null;
-
-const getLock = async () => {
-    // TODO: May want to pass a key in so this can be used for more than just 1 kind of task.
-    if (!lock) {
-        lock = createLock({
-            adapter: new NodeRedisAdapter(await getRedisClient()),
-            key: LOCK_REDIS_KEY,
-            ttl: REDIS_CACHE_LOCK_TTL_MS,
-            // For our use-case, if the lock is already acquired, then the current process
-            // (presumably another node instance) should skip attempting to cache all the products.
-            retryAttempts: 0,
-            retryDelay: 0,
-        });
-    }
-    return lock;
+const getLock = async (windowKey: number) => {
+    // TODO: May want to allow passing in more generic keys so this can be used for more than just 1 kind of task.
+    return createLock({
+        adapter: new NodeRedisAdapter(await getRedisClient()),
+        key: LOCK_REDIS_KEY + `:${windowKey}`,
+        ttl: REDIS_CACHE_LOCK_TTL_MS,
+        // For our use-case, if the lock is already acquired, then the current process
+        // (presumably another node instance) should skip attempting to cache all the products.
+        retryAttempts: 0,
+        retryDelay: 0,
+    });
 };
 
 export const lockAndRunTask = async (task: () => Promise<void>) => {
     // See here for Redis docs on lock stuff: https://redis.io/docs/latest/commands/set/#patterns
     // Info on Redis distributed locks: https://redis.io/docs/latest/develop/clients/patterns/distributed-locks/
     // Info on redlock-universal: https://github.com/alexpota/redlock-universal
-    const lock = await getLock();
 
-    let handle;
+    // This windowStart variable is to set a window on the singular execution needed so other node instances
+    // don't run within the same window of time.
+    const windowStart =
+        Math.floor(Date.now() / REDIS_CACHE_LOCK_TTL_MS) *
+        REDIS_CACHE_LOCK_TTL_MS;
+    const lock = await getLock(windowStart);
+
     try {
-        handle = await lock.acquire();
+        await lock.acquire();
     } catch (_err) {
         console.log(
             "Lock not acquired, another process is taking care of this. Skipping..."
@@ -39,6 +39,9 @@ export const lockAndRunTask = async (task: () => Promise<void>) => {
     try {
         await task();
     } finally {
-        await lock.release(handle);
+        // Normally we'd release the lock in a `finally` statement, however, since we only want 1 instance
+        // of node to cache all the products, we'll keep the lock set until its expiration. It will try
+        // again at the next interval in app.ts.
+        // await lock.release(handle);
     }
 };
